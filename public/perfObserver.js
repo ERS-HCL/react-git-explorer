d0e0d7cf47e4a4a8e05d2864b9d5abb173a4a55a
Tarun Kumar Sukhu <tksukhu@gmail.com>
2018-08-23 14:50:53 +0530

    Added Perf Observer

---
+++ public/perfObserver.js
@@ -0,0 +1,32 @@
+function initPerformanceObserver() {
+  window._observer = new window.PerformanceObserver(list => {
+    list.getEntries().forEach(entry => {
+      // Display each reported measurement on console
+      if (console) {
+        console.log(
+          'Name: ' +
+            entry.name +
+            ', Type: ' +
+            entry.entryType +
+            ', Start: ' +
+            entry.startTime +
+            ', Duration: ' +
+            entry.duration +
+            '\n'
+        );
+      }
+    });
+  });
+  console.log('Observe longtask');
+  window._observer.observe({
+    entryTypes: [
+      'resource',
+      'mark',
+      'measure',
+      'paint',
+      'frame',
+      'navigation',
+      'longtask'
+    ]
+  });
+}
